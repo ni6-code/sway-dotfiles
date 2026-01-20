b96df55bfcf64072e0c8a67c7debac2269407879
ni6-code <196664712+ni6-code@users.noreply.github.com>
2026-01-20 22:04:57 +0300

    Added i3-foot.ini, refactored calendar.sh
    Now cursor in drop down calendar window is invisible

--- .config/i3blocks/calendar.sh
+++ .config/i3blocks/calendar.sh
@@ -3,5 +3,5 @@
 echo "$(date +'%a %b %-d, %H:%M:%S')"
 
 if [ ! -z "$BLOCK_BUTTON" ]; then
-    footclient -T "i3-calendar" -e sh -c "cal -my; read -n 1 -s"
+    foot -c "$HOME/.config/foot/i3-foot.ini" -T "i3-calendar" -e sh -c "cal -my; read -n 1 -s"
 fi
